e183dff1de6d885135fb7d9a82c984bff89e114f
Hugo Josefson <hugo@josefson.org>
2024-06-10 20:19:14 +0200

    feat: --force

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/bin/sh
-// 2>/dev/null;DENO_VERSION_RANGE="^1.42.1";DENO_RUN_ARGS="--allow-all";set -e;V="$DENO_VERSION_RANGE";A="$DENO_RUN_ARGS";h(){ [ -x "$(command -v "$1" 2>&1)" ];};g(){ u="$([ "$(id -u)" != 0 ]&&echo sudo||:)";if h brew;then echo "brew install $1";elif h apt;then echo "($u apt update && $u DEBIAN_FRONTEND=noninteractive apt install -y $1)";elif h yum;then echo "$u yum install -y $1";elif h pacman;then echo "$u pacman -yS --noconfirm $1";elif h opkg-install;then echo "$u opkg-install $1";fi;};p(){ q="$(g "$1")";if [ -z "$q" ];then echo "Please install '$1' manually, then try again.">&2;exit 1;fi;eval "o=\"\$(set +o)\";set -x;$q;set +x;eval \"\$o\"">&2;};f(){ h "$1"||p "$1";};w(){ [ -n "$1" ] && "$1" -V >/dev/null 2>&1;};U="$(l=$(printf "%s" "$V"|wc -c);for i in $(seq 1 $l);do c=$(printf "%s" "$V"|cut -c $i);printf '%%%02X' "'$c";done)";D="$(w "$(command -v deno||:)"||:)";t(){ i="$(if h findmnt;then findmnt -Ononoexec,noro -ttmpfs -nboAVAIL,TARGET|sort -rn|while IFS=$'\n\t ' read -r a m;do [ "$a" -ge 150000000 ]&&[ -d "$m" ]&&printf %s "$m"&&break||:;done;fi)";printf %s "${i:-"${TMPDIR:-/tmp}"}";};s(){ deno eval "import{satisfies as e}from'https://deno.land/x/semver@v1.4.1/mod.ts';Deno.exit(e(Deno.version.deno,'$V')?0:1);">/dev/null 2>&1;};e(){ R="$(t)/deno-range-$V/bin";mkdir -p "$R";export PATH="$R:$PATH";s&&return;f curl;v="$(curl -sSfL "https://semver-version.deno.dev/api/github/denoland/deno/$U")";i="$(t)/deno-$v";ln -sf "$i/bin/deno" "$R/deno";s && return;f unzip;([ "${A#*-q}" != "$A" ]&&exec 2>/dev/null;curl -fsSL https://deno.land/install.sh|DENO_INSTALL="$i" sh -s $DENO_INSTALL_ARGS "$v"|grep -iv discord>&2);};e;exec deno run $A "$0" "$@"
+// 2>/dev/null;DENO_VERSION_RANGE="^1.44.1";DENO_RUN_ARGS="--allow-all";set -e;V="$DENO_VERSION_RANGE";A="$DENO_RUN_ARGS";h(){ [ -x "$(command -v "$1" 2>&1)" ];};g(){ u="$([ "$(id -u)" != 0 ]&&echo sudo||:)";if h brew;then echo "brew install $1";elif h apt;then echo "($u apt update && $u DEBIAN_FRONTEND=noninteractive apt install -y $1)";elif h yum;then echo "$u yum install -y $1";elif h pacman;then echo "$u pacman -yS --noconfirm $1";elif h opkg-install;then echo "$u opkg-install $1";fi;};p(){ q="$(g "$1")";if [ -z "$q" ];then echo "Please install '$1' manually, then try again.">&2;exit 1;fi;eval "o=\"\$(set +o)\";set -x;$q;set +x;eval \"\$o\"">&2;};f(){ h "$1"||p "$1";};w(){ [ -n "$1" ] && "$1" -V >/dev/null 2>&1;};U="$(l=$(printf "%s" "$V"|wc -c);for i in $(seq 1 $l);do c=$(printf "%s" "$V"|cut -c $i);printf '%%%02X' "'$c";done)";D="$(w "$(command -v deno||:)"||:)";t(){ i="$(if h findmnt;then findmnt -Ononoexec,noro -ttmpfs -nboAVAIL,TARGET|sort -rn|while IFS=$'\n\t ' read -r a m;do [ "$a" -ge 150000000 ]&&[ -d "$m" ]&&printf %s "$m"&&break||:;done;fi)";printf %s "${i:-"${TMPDIR:-/tmp}"}";};s(){ deno eval "import{satisfies as e}from'https://deno.land/x/semver@v1.4.1/mod.ts';Deno.exit(e(Deno.version.deno,'$V')?0:1);">/dev/null 2>&1;};e(){ R="$(t)/deno-range-$V/bin";mkdir -p "$R";export PATH="$R:$PATH";s&&return;f curl;v="$(curl -sSfL "https://semver-version.deno.dev/api/github/denoland/deno/$U")";i="$(t)/deno-$v";ln -sf "$i/bin/deno" "$R/deno";s && return;f unzip;([ "${A#*-q}" != "$A" ]&&exec 2>/dev/null;curl -fsSL https://deno.land/install.sh|DENO_INSTALL="$i" sh -s $DENO_INSTALL_ARGS "$v"|grep -iv discord>&2);};e;exec deno run $A "$0" "$@"
+import { parseArgs } from "jsr:@std/cli@0.224/parse-args";
 import { getCurrentTime } from "./get-current-time.ts";
 import { isDatasetLocked } from "./is-dataset-locked.ts";
 import { log, logNoTimestamp } from "./log.ts";
@@ -8,32 +9,41 @@ import { runAnyDotUnlockrcInDataset } from "./run-any-dot-unlockrc-in-dataset.ts
 import { unlockDataset } from "./unlock-dataset.ts";
 import { waitUntilPossibleShutdown } from "./wait-until-possible-shutdown.ts";
 import { waitUntilSystemStateReady } from "./wait-until-system-state-ready.ts";
-import addSignalListener = Deno.addSignalListener;
 
 export function getUsage(): string {
   return `
-Usage: ${import.meta.filename} <dataset_id> <ssh_args_and_user_at_host>
+Usage: ${import.meta.filename} [-f | --force] <dataset_id> <ssh_args_and_user_at_host>
 
 Example: ${import.meta.filename} my-dataset "user@host"
 `;
 }
 
-export async function main(args: string[]): Promise<void> {
+export async function main(rawArgs: string[]): Promise<void> {
   let keepGoing = true;
 
-  addSignalListener("SIGINT", () => {
+  Deno.addSignalListener("SIGINT", () => {
     logNoTimestamp();
     log("You pressed CTRL+C, so exiting.");
     keepGoing = false;
     Deno.exit(130);
   });
 
-  if (args.some((a) => ["-h", "--help"].includes(a))) {
+  const args = parseArgs(rawArgs, {
+    boolean: ["force", "help"],
+    string: ["_"],
+    collect: ["_"],
+    alias: {
+      f: "force",
+      h: "help",
+    },
+  });
+
+  if (args.help) {
     console.log(getUsage());
     Deno.exit(0);
   }
 
-  const [datasetId, ...sshArgsAndUserAtHost] = args;
+  const [datasetId, ...sshArgsAndUserAtHost] = args._;
   if (
     !datasetId || !sshArgsAndUserAtHost || sshArgsAndUserAtHost.length === 0
   ) {
@@ -61,7 +71,12 @@ export async function main(args: string[]): Promise<void> {
       );
       if (bootTime > (unlockTime ?? "0")) {
         log("boot_time is later than unlock_time, so we are on a new boot.");
-        await unlockDataset(datasetId, passphrase, sshArgsAndUserAtHost);
+        await unlockDataset(
+          datasetId,
+          passphrase,
+          sshArgsAndUserAtHost,
+          args.force,
+        );
         unlockTime = getCurrentTime();
         log(`unlock_time: ${unlockTime} (we unlocked, just now)`);
         await runAnyDotUnlockrcInDataset(datasetId, sshArgsAndUserAtHost);
--- src/unlock-dataset.ts
+++ src/unlock-dataset.ts
@@ -5,12 +5,13 @@ export async function unlockDataset(
   datasetId: string,
   passphrase: string,
   sshArgsAndUserAtHost: string[],
+  force = false,
 ): Promise<void> {
   log(`Unlocking dataset "${datasetId}"...`);
   logNoTimestampNoNewline(
     await runViaSshTruenasCli(
       sshArgsAndUserAtHost,
-      `storage dataset unlock id="${datasetId}" unlock_options={"recursive":true, "key_file":false, "force":false, "datasets":[{"name":"${datasetId}", "passphrase":"${passphrase}"}]}`,
+      `storage dataset unlock id="${datasetId}" unlock_options={"recursive":true, "key_file":false, "force":${force}, "datasets":[{"name":"${datasetId}", "passphrase":"${passphrase}"}]}`,
     ),
   );
 }
